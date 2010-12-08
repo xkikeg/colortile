728f9469cb2cac17005f8c0395957ac97e8e332a
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2010-12-08 15:54:41 +0900

    Implement Depth First Search

--- colortile.py
+++ colortile.py
@@ -124,8 +124,76 @@ class ColorTileArray(object):
         return "\n".join("%s: %d" % (str_tilecolor(i), result[i]) for i in range(0, len(result)))
 
 
+    def is_good_state(self):
+        counts = self.count_all()[1:]
+        if len(filter(lambda x: x == 1, counts)) > 0: return False
+        return True
+
+    def is_solved(self):
+        return len(reduce(lambda a,b: a+b, self.array)) == self.count(BG)
+
+    def get_neighbor(self, iter):
+        for i,color in enumerate(iter):
+            if is_tile(color): return (i+1, color)
+        return (None, None)
+
+    def legal_moves(self):
+        moves = []
+        for i in range(0, len(self.array)):
+            for j in range(0, len(self.array[i])):
+                if is_tile(self.array[i][j]): continue
+                column = [ self.array[k][j] for k in range(0, len(self.array)) ]
+                row = self.array[i]
+                u_idx, u_color = self.get_neighbor(reversed(column[:i]))
+                d_idx, d_color = self.get_neighbor(column[i+1:])
+                l_idx, l_color = self.get_neighbor(reversed(row[:j]))
+                r_idx, r_color = self.get_neighbor(row[j+1:])
+                u = None if u_idx is None else (u_color, ((i - u_idx, j), ))
+                d = None if d_idx is None else (d_color, ((i + d_idx, j), ))
+                l = None if l_idx is None else (l_color, ((i, j - l_idx), ))
+                r = None if r_idx is None else (r_color, ((i, j + r_idx), ))
+                tileplus = lambda x, y: [(x[0], x[1]+y[1])] if x[0] == y[0] else [x, y]
+                tiles = filter(lambda a: len(a[1]) >= 2,
+                               reduce(lambda a,b: a[:-1] + tileplus(a[-1], b) if len(a) > 0 else [b],
+                                      sorted(filter(lambda x: x is not None, (u,d,l,r))), []))
+                len(tiles) > 0 and moves.append(((i, j), tiles))
+        return moves
+
+    def search_depth_first(self):
+        assert len(self.array) > 0, "Array is not filled"
+        # If this state is not good, then return
+        if not self.is_good_state(): return ()
+        # Get legal moves, and return if there are no moves
+        moves = self.legal_moves()
+        if len(moves) == 0: return ()
+        # See all moves
+        for move, tiles in moves:
+            # Drop tiles in this move
+            for color, d_pos in tiles:
+                for i in d_pos:
+                    self.array[i[0]][i[1]] = BG
+            # If there are no tiles, return move
+            # Else recursively call search_depth_first()
+            print " ".join(str(i) for i in self.count_all()), move, tiles
+            if self.is_solved():
+                retval = (move,)
+            else:
+                branch = self.search_depth_first()
+                retval = (move,) + branch if len(branch) > 0 else ()
+            # Restore dropped tiles
+            for color, d_pos in tiles:
+                for i in d_pos:
+                    self.array[i[0]][i[1]] = color
+            # Return move if this is valid route
+            if len(retval) > 0:
+                return retval
+        # End of seaching all moves
+        return ()
+
+
 if __name__=="__main__":
     x = ColorTileArray()
     x.fixed_image_loader(sys.argv[1])
     print x
     print x.str_count_all()
+    print x.search_depth_first()
